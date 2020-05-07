7f721f3a5341be677fbc74ab94dc57d70496f93d
eddyphilippo <ewp972@gmail.com>
2020-05-07 14:23:00 +0200

    correction

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//On est prêt à interpréter du jquery
+$(function(){
+  //Je cible les enfants (.children) de l'élément 'ol' et je modifie le css, en indiquant dans les parenthèse le propriété 'color' et sa valeur 'red'
+$('ol').children().css('color','red');
+});
